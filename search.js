2e703206361501f05b697425470c5625537048d1
IFEANYI IBIAM IFEGWU <144519897+IFY1237IBIAM@users.noreply.github.com>
2025-02-04 19:47:02 +0100

    Add files via upload

---
+++ search.js
@@ -0,0 +1,13 @@
+// Handle search form submission
+document.getElementById("searchForm").addEventListener("submit", (e) => {
+    e.preventDefault(); // Prevent the form from refreshing the page
+  
+    // Get the search query
+    const query = document.getElementById("searchInput").value.trim();
+  
+    // Redirect to the search results page with the query as a parameter
+    if (query) {
+      window.location.href = `product-list.html?query=${encodeURIComponent(query)}`;
+    }
+  });
+  
